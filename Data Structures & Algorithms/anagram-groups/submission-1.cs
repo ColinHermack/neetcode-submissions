b5f6fdf3afff228a0c57359083d917f2ea0a3cc0
Colin Hermack <chermack@purdue.edu>
2026-08-09 12:45:42 -0400

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public List<List<string>> GroupAnagrams(string[] strs) {
+        var groups = new Dictionary<string, List<string>>();
+        foreach (string str in strs) {
+            var keyArr = new int[26];
+            foreach (char c in str) {
+                keyArr[(int)c - 97]++;
+            }
+            string keyStr = string.Join("", keyArr.Select(n => n.ToString("X")));
+            if (groups.Keys.ToList().Contains(keyStr)) {
+                groups[keyStr].Add(str);
+            } else {
+                groups[keyStr] = new List<string>();
+                groups[keyStr].Add(str);
+            }
+        }
+        List<List<string>> retVal = new();
+        foreach (string key in groups.Keys) {
+            retVal.Add(groups[key]);
+        }
+        return retVal;
+    }
+}
