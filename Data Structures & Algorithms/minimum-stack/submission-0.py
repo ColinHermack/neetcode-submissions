60292526fd9285ad19fda5c13439f709f9f546ff
Colin Hermack <chermack@purdue.edu>
2026-09-23 13:08:07 -0400

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.py
@@ -0,0 +1,25 @@
+class MinStack:
+    
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        # Case when the stack is empty
+        if len(self.stack) == 0:
+            self.stack.append(val)
+            self.minStack.append(val)
+        else:
+            self.stack.append(val)
+            self.minStack.append(min(val, self.minStack[-1]))
+
+    def pop(self) -> None:
+        self.minStack.pop()
+        return self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
+        
